d5994bbb852370b70aca79e0e688ef8ec413d1bb
Balakrishnan M <163383087+Bala-self@users.noreply.github.com>
2026-06-01 13:26:17 +0530

    Update ProtectedRoute.jsx

--- frontent/src/components/ProtectedRoute.jsx
+++ frontent/src/components/ProtectedRoute.jsx
@@ -5,8 +5,15 @@ const ProtectedRoute = ({ children , adminOnly = false }) => {
     const { user, loading } = useAuth();
 
     if (loading) {
-        return <div>Loading...</div>;
-    }
+  return (
+    <div className="flex items-center justify-center min-h-screen space-x-2">
+      <div className="w-2 h-2 bg-red-500 rounded-full animate-bounce"></div>
+      <div className="w-2 h-2 bg-red-500 rounded-full animate-bounce delay-150"></div>
+      <div className="w-2 h-2 bg-red-500 rounded-full animate-bounce delay-300"></div>
+    </div>
+  );
+}
+
 
     if (!user) {
         return <Navigate to="/auth" replace />;
